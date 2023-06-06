4bbcf0efca4ed1329c189865a3ac61a087b2922e
sad_bred#6650 <117604119+sadbred@users.noreply.github.com>
2023-06-05 23:36:25 -0500

    Update action_page.php

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+// Do any necessary processing with the data
+
+// Echo back the data as the response
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email;
+?>
