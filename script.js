54708c3dbe6f84aeec293f924b8623c7615a1c6f
sad_bred#6650 <117604119+sadbred@users.noreply.github.com>
2023-06-06 23:36:54 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  const downloadButton = document.getElementById('downloadButton');
-  downloadButton.addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = 'https://raw.githubusercontent.com/sadbred/sadbred.github.io/main/img/20230606_161702.jpg';
-    link.download = '20230606_161702.jpg';
-    link.click();
-  });
-})();
