e408d1e57ef01da6130f5b29b6ad4d0ebc22d4fd
sad_bred#6650 <117604119+sadbred@users.noreply.github.com>
2023-08-16 21:22:53 -0500

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,23 @@
+// Set the target date and time for the countdown
+const targetDate = new Date("2023-08-19T12:00:00").getTime();
+
+// Update the countdown every second
+const countdownInterval = setInterval(function() {
+  const now = new Date().getTime();
+  const timeRemaining = targetDate - now;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days.toString().padStart(2, "0");
+  document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
+  document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
+  document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
+
+  if (timeRemaining < 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("countdown").textContent = "Games are here!";
+  }
+}, 1000);
