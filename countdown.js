860ffa88286c14165bab218fe81466b28bb8cc9c
sad_bred#6650 <117604119+sadbred@users.noreply.github.com>
2023-08-17 08:18:44 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Set the target date and time for the countdown
-const targetDate = new Date("2023-08-17T12:00:00").getTime();
+const targetDate = new Date("2023-08-19T12:00:00").getTime();
 
 // Update the countdown every second
 const countdownInterval = setInterval(function() {
